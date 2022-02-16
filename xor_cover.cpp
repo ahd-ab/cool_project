d61181b707372560a97b6f24979252c3fc399224
Amit Yakovian <amit.yakovian@gmail.com>
2022-02-16 17:26:07 +0200

    Added xor cover

---
+++ xor_cover.cpp
@@ -0,0 +1,14 @@
+#include "icover.h"
+#include "xor_cover.h"
+
+
+XorCover::XorCover(std::unique_ptr<ICover> inner_cover) :m_inner_cover(std::move(inner_cover)) {};
+
+void XorCover::wrap(const std::vector<char> &data) {
+        std::transform(data.begin(), data.end(), data.begin(), [this](char byte){return byte | m_key;});
+        m_inner_cover->wrap(data);
+    }
+
+
+
+
---
+++ xor_cover.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef COOL_PROJECT_XOR_COVER_H
+#define COOL_PROJECT_XOR_COVER_H
+
+#endif //COOL_PROJECT_XOR_COVER_H
+
+
+#include <memory>
+#include <algorithm>
+
+#include "icover.h"
+
+
+class XorCover: public ICover {
+private:
+    std::unique_ptr<ICover> m_inner_cover;
+    char m_key = 'a';
+
+public:
+    explicit XorCover(std::unique_ptr<ICover> inner_cover);
+
+    void wrap(const std::vector<char> &data) override;
+
+};
