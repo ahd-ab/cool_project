054a7e69f93beed8ad38171d9b0e5252ee62cc1d
Amit Yakovian <amit.yakovian@gmail.com>
2022-02-17 12:46:11 +0200

    Added untested implementation of wrap()

---
+++ udp_cover.cpp
@@ -0,0 +1,25 @@
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "udp_cover.h"
+
+UDPCover::UDPCover() {
+    m_sock = socket(AF_INET, SOCK_DGRAM, 0);
+}
+
+UDPCover::~UDPCover() {
+    close(m_sock);
+}
+
+void UDPCover::wrap(const std::vector<char> &data) {
+
+    struct sockaddr_in target_address;
+
+    target_address.sin_family = AF_INET;
+    target_address.sin_port = m_target_port;
+    inet_aton(m_target_ip_address.c_str(), &target_address.sin_addr);
+
+    size_t sending_res = sendto(m_sock, data, data.size(), 0, (struct sockaddr*) &target_address, sizeof(target_address));
+
+}
--- udp_cover.h
+++ udp_cover.h
@@ -1,10 +1,27 @@
-
+#pragma once
 #include "icover.h"
+#include <string>
 
+
+#define TARGET_IP "127.0.0.1"
+#define TARGET_PORT 11111
 
 class UDPCover: public ICover {
 
 public:
 
+    explicit UDPCover();
+    ~UDPCover();
+    UDPCover(const UDPCover&) = delete;
+    UDPCover(const UDPCover&&) = delete;
+    const UDPCover& operator=(const UDPCover&) = delete;
 
+    virtual void wrap(const std::vector<char>& data) = 0;
+
+    virtual std::vector<char> unwrap(const std::vector<char> &data) = 0;
+
+private:
+    int m_sock;
+    std::string m_target_ip_address = TARGET_IP;
+    int m_target_port = TRAGET_PORT;
 };
